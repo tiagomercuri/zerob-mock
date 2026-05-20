f54eb0948288b323cbd954397d4cee3b417286a0
Tiago Mercuri <tiagomercuri@gmail.com>
2026-05-20 03:28:06 -0300

    first commit

---
+++ middleware.js
@@ -0,0 +1,12 @@
+// Adiciona CORS para o frontend Angular em localhost:4200
+module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+};
